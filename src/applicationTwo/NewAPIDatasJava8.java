a243ad2f4856100f67e0445e04367125594eba45
Vinicius <vinicius.tecbcc@gmail.com>
2020-02-21 19:03:18 -0300

    Estudando mais sobre a nova api de datas

--- src/applicationTwo/NewAPIDatasJava8.java
+++ src/applicationTwo/NewAPIDatasJava8.java
@@ -1,14 +1,24 @@
 package applicationTwo;
 
+import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
+import java.time.MonthDay;
 import java.time.Period;
+import java.time.Year;
+import java.time.YearMonth;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Locale;
 
 public class NewAPIDatasJava8 {
 	
@@ -78,6 +88,106 @@ public class NewAPIDatasJava8 {
 		/**
 		 * Com um ZonedDateTime, podemos representar, por exemplo, a data de um voo.
 		 */
+		
+		ZoneId fusoHorarioDeSaoPauloo = ZoneId.of("America/Sao_Paulo");
+		ZoneId fusoHorarioDeNovaYork = ZoneId.of("America/New_York");
+		
+		LocalDateTime saidaDeSaoPauloSemFusoHorario = 
+				LocalDateTime.of(2020, Month.APRIL, 4, 22, 30);
+		LocalDateTime chegadaEmNovaYorkSemFusoHorario =
+				LocalDateTime.of(2020, Month.APRIL, 5, 7, 10);
+		
+		ZonedDateTime saidaDeSaoPauloComFusoHorario = ZonedDateTime.of(saidaDeSaoPauloSemFusoHorario,
+				fusoHorarioDeSaoPauloo);
+		System.out.println(saidaDeSaoPauloComFusoHorario);
+
+		ZonedDateTime chegadaEmNovaYorkComFusoHorario = ZonedDateTime.of(chegadaEmNovaYorkSemFusoHorario,
+				fusoHorarioDeNovaYork);
+		System.out.println(chegadaEmNovaYorkComFusoHorario);
+		
+		Duration duracaoDoVoo = Duration.between(saidaDeSaoPauloComFusoHorario, chegadaEmNovaYorkComFusoHorario);
+		System.out.println(duracaoDoVoo);
+		
+		/**
+		 * Se calcularmos de maneira ingênua a duração do voo,
+		 * teríamos 8:40. Porém, como há uma diferença entre os fusos horários de São Paulo
+		 * e Nova York, a duração correta é 9:40. Repare que a API já faz o tratamento
+		 * de fusos horários distintos.
+		 */
+		
+		/**
+		 * Outro cuidado importante que devemos ter é em relação ao horário de verão.
+		 * No fim do horário de verão, por exemplo, a mesma hora existe duas vezes!
+		 */
+		
+		LocalDateTime fimDoHorarioDeVerao2020SemFusoHorario =
+				LocalDateTime.of(2020, Month.FEBRUARY, 15, 23,00);
+		
+		ZonedDateTime fimDoHorarioDeVerao2020ComFusoHOrario = 
+				fimDoHorarioDeVerao2020SemFusoHorario.atZone(fusoHorarioDeSaoPauloo);
+		System.out.println(fimDoHorarioDeVerao2020ComFusoHOrario);
+		
+		ZonedDateTime maisUmaHora = fimDoHorarioDeVerao2020ComFusoHOrario.plusHours(1);
+		System.out.println(maisUmaHora);
+		
+		
+		/**
+		 * Existem também as classes MonthDay, que deve ser utilizada para
+		 * representar datas importantes que se repetem todos os anos,
+		 * e YearMonth, que deve ser utilizada para representar um mês
+		 * inteiro de um ano específico.
+		 */
+		
+		MonthDay natal = MonthDay.of(Month.DECEMBER, 25);
+		YearMonth copaDoMundo2014 = YearMonth.of(2014, Month.JUNE);
+		
+		System.out.println(natal + "\n" + copaDoMundo2014);
+		
+		/**
+		 * O toString padrão das classes da API utiliza o formato ISO-8601.
+		 * Se quisermos definir o formato de apresentação da data,
+		 * devemos utilizar o método format, passando um DateTimeFormatter.
+		 */
+		
+		DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		hoje.format(formatador);
+		
+		/**
+		 * O enum FormatStyle possui alguns formatos pré-definidos,
+		 * que podem ser combinados com um Locale.
+		 */
+		
+		LocalDateTime agoraaa = LocalDateTime.now();
+		formatador = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT).withLocale(new Locale("pt", "br"));
+		System.out.println(agoraaa.format(formatador));
+		
+		/**
+		 * Todas as classes mencionadas possuem diversos métodos que permitem manipular as medidas de tempo.
+		 * Por exemplo, podemos usar o método plusDays da classe LocalDate para aumentarmos um dia:
+		 */
+		
+		LocalDate hojee = LocalDate.now();
+		LocalDate amanha = hoje.plusDays(1);
+		System.out.println(hojee);
+		System.out.println(amanha);
+		
+		/**
+		 * Outro cálculo interessante é o número de medidas de tempo
+		 * até uma determinada data, que podemos fazer através do método until. Para
+		 * descobrir o número de dias até uma data, por exemplo,
+		 * devemos passar ChronoUnit.DAYS como parâmetro.
+		 */
+		LocalDate natalDesseAno = natal.atYear(Year.now().getValue());
+		long diasAteONatal = LocalDate.now().until(natalDesseAno, ChronoUnit.DAYS);
+		System.out.println(diasAteONatal);
+		
+		/**
+		 * Ajustando pra proxima sexta como exemplo:
+		 */
+		TemporalAdjuster ajustadorParaProximaSexta = TemporalAdjusters.next(DayOfWeek.FRIDAY);
+		LocalDate proximaSexta = LocalDate.now().with(ajustadorParaProximaSexta);
+		System.out.println(proximaSexta);
+		
 		
 	}
 	
